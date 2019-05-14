b5ea91494301f91814ba6610f6142fa6866b4729
Nicolas Maitre <nicolas.johan.maitre@gmail.com>
2019-05-14 14:19:41 +0200

    fixed sql data script

--- code/server_resources/sql_scripts/default_data.sql
+++ code/server_resources/sql_scripts/default_data.sql
@@ -1,23 +1,23 @@
-USE messaging_web_app_db
+USE messaging_web_app_db;
 -- DEFAULT DATA
 INSERT INTO users		SET id='0000-0000-0000-0000-0000', pseudo='nmaitre', email='nicolas.maitre@cpnv.ch', first_name='Nicolas', last_name='Maitre', creation_time=CURRENT_TIMESTAMP(), active=1, enabled=1;
 INSERT INTO users		SET id='1111-1111-1111-1111-1111', pseudo='nglassey', email='nicolas.glassey@cpnv.ch', first_name='Nicolas', last_name='Glassey', creation_time=CURRENT_TIMESTAMP(), active=1, enabled=1;
 INSERT INTO users		SET id='2222-2222-2222-2222-2222', pseudo='ggruaz', email='ggruaz@gmail.com', first_name='Gilbert', last_name='Gruaz', creation_time=CURRENT_TIMESTAMP(), active=1, enabled=1;
 INSERT INTO users		SET id='3333-3333-3333-3333-3333', pseudo='jlagona', email='julien.lagona@gmail.com', first_name='Julien', last_name='Lagona', creation_time=CURRENT_TIMESTAMP(), active=1, enabled=1;
 
---friends
+-- friends
 INSERT INTO friends		SET user_0="bb686737-5080-11e9-809c-b827eb4f1633", user_1="f319ca59-5080-11e9-809c-b827eb4f1633", creation_time=CURRENT_TIMESTAMP(), active=1;
 
---private
+-- private
 INSERT INTO groups 		SET id="0000-1111-2222-3333-4444", creation_time=CURRENT_TIMESTAMP(), name="Nicolas Maitre, Nicolas Glassey", type="private", active=1;
-INSERT INTO user_groups SET user="0000-0000-0000-0000-0000", group="0000-1111-2222-3333-4444", creation_time=CURRENT_TIMESTAMP(), active=1;
-INSERT INTO user_groups SET user="1111-1111-1111-1111-1111", group="0000-1111-2222-3333-4444", creation_time=CURRENT_TIMESTAMP(), active=1;
---group
+INSERT INTO user_groups SET user="0000-0000-0000-0000-0000",`group`="0000-1111-2222-3333-4444", creation_time=CURRENT_TIMESTAMP(), active=1;
+INSERT INTO user_groups SET user="1111-1111-1111-1111-1111",`group`="0000-1111-2222-3333-4444", creation_time=CURRENT_TIMESTAMP(), active=1;
+-- group
 INSERT INTO groups 		SET id="5555-6666-7777-8888-9999", creation_time=CURRENT_TIMESTAMP(), name="Le groupe CPNV", type="group", active=1;
-INSERT INTO user_groups SET user="0000-0000-0000-0000-0000", group="5555-6666-7777-8888-9999", creation_time=CURRENT_TIMESTAMP(), active=1;
-INSERT INTO user_groups SET user="1111-1111-1111-1111-1111", group="5555-6666-7777-8888-9999", creation_time=CURRENT_TIMESTAMP(), active=1;
-INSERT INTO user_groups SET user="2222-2222-2222-2222-2222", group="5555-6666-7777-8888-9999", creation_time=CURRENT_TIMESTAMP(), active=1;
-INSERT INTO user_groups SET user="3333-3333-3333-3333-3333", group="5555-6666-7777-8888-9999", creation_time=CURRENT_TIMESTAMP(), active=1;
+INSERT INTO user_groups SET user="0000-0000-0000-0000-0000",`group`="5555-6666-7777-8888-9999", creation_time=CURRENT_TIMESTAMP(), active=1;
+INSERT INTO user_groups SET user="1111-1111-1111-1111-1111",`group`="5555-6666-7777-8888-9999", creation_time=CURRENT_TIMESTAMP(), active=1;
+INSERT INTO user_groups SET user="2222-2222-2222-2222-2222",`group`="5555-6666-7777-8888-9999", creation_time=CURRENT_TIMESTAMP(), active=1;
+INSERT INTO user_groups SET user="3333-3333-3333-3333-3333",`group`="5555-6666-7777-8888-9999", creation_time=CURRENT_TIMESTAMP(), active=1;
 
 INSERT INTO tokens 		SET id=UUID(), value='1234-1234-1234-1234-1234', type='session', expiration_time=0, active=1, user='0000-0000-0000-0000-0000';
 INSERT INTO tokens 		SET id=UUID(), value='2345-2345-2345-2345-2345', type='session', expiration_time=0, active=1, user='1111-1111-1111-1111-1111';
